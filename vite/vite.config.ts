4acf6045fc58d617abbc93534c7341afd23f4b70
Justin R. Evans <jurevans@gmail.com>
2025-08-13 05:30:32 -0400

    Reference new SDK-JS plugin name

--- vite/vite.config.ts
+++ vite/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import react from "@vitejs/plugin-react";
-import { sdkMulticoreWorkerHelpers } from "@namada/sdkjs-esbuild-plugin";
+import { sdkMulticoreWorkerHelpers } from "@namada/vite-esbuild-plugin";
 
 export default defineConfig({
   server: {
