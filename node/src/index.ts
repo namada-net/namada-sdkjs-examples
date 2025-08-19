b730b7ea53f8938683ea35065a9dd0601f4496e9
Justin R. Evans <jurevans@gmail.com>
2025-08-19 12:37:29 -0400

    feat: adding more node sdk stuff

--- node/src/index.ts
+++ node/src/index.ts
@@ -1,1 +1,22 @@
-console.log("IT WORKS!");
+import { initSdk } from "@namada/sdk-node";
+import init from "@namada/sdk-node/init";
+
+const initializeSdk = async (): Promise<unknown> => {
+  const { memory } = init();
+  console.log("Initialized wasm memory:", memory);
+
+  const rpcUrl = "https://rpc.housefire.tududes.com";
+  const token = "tnam1q9gr66cvu4hrzm0sd5kmlnjje82gs3xlfg3v6nu7";
+  const maspIndexerUrl = "https://masp.housefire.tududes.com";
+  const dbName = "testDb";
+
+  const sdk = await initSdk({ rpcUrl, token, maspIndexerUrl, dbName });
+  return sdk;
+};
+
+const app = async () => {
+  const sdk = await initializeSdk();
+  console.log({ sdk });
+};
+
+app().then(() => console.log("IT WORKS!"));
