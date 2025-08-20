cea463a63fcfa1e59c5c01d9d4a97c729473018a
Justin R. Evans <jurevans@gmail.com>
2025-08-20 06:27:48 -0400

    feat: provide both multicore and single core vite examples

--- vite-multicore/src/utils/sdk.ts
+++ vite-multicore/src/utils/sdk.ts
@@ -1,5 +1,5 @@
-import { initSdk } from "@namada/sdk/inline";
-import { Sdk } from "@namada/sdk";
+import { initSdk } from "@namada/sdk-multicore/inline";
+import { Sdk } from "@namada/sdk-multicore";
 
 const initializeSdk = async (): Promise<Sdk> => {
   const rpcUrl = "https://rpc.housefire.tududes.com";
